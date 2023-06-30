a503698566c528bd9a98cde8db1363719541a27e
Nessa515 <93387954+Nessa515@users.noreply.github.com>
2023-06-30 18:11:53 -0400

    feat: seperation of port command in a server file

--- src/app.ts
+++ src/app.ts
@@ -11,27 +11,14 @@ import{notFound} from './errors/not-found';
 
 
 
-// middleware
+
 app.use(express.json());
 
 // Route
 app.use('/', router);
 
+// middleware
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-
-// Porta
-const port = process.env.PORT || 3000
-
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI!)
-        app.listen(port, () => console.log(`Server is listening port ${port}...`))
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-start()
+export{app}
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { app } from "./app";
+import { connectDB } from "./database/connect";
+
+// Porta
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI!)
+        app.listen(port, () => console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
